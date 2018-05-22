3ea01c366a968dbcc63a0940c3af77d208a1dee4
Vasiliy Makarov <drmoriarty.0@gmail.com>
2018-05-22 09:25:12 +0300

    update

--- Classes/AdMob.cpp
+++ Classes/AdMob.cpp
@@ -24,15 +24,71 @@ static void printLog(const char* str) {
 //
 ///////////////////////////////////////
 
+firebase::admob::AdParent getAdParent() {
+    // Returns the Android Activity.
+    return cocos2d::JniHelper::getActivity();
+}
+
+static void InterstitialInitCallback(const firebase::Future<void>& future, void* user_data) {
+    // Called when the Future is completed for the last call to the InterstitialAd::Initialize()
+    // method. If the error code is firebase::admob::kAdMobErrorNone, then you're ready to
+    // load the interstitial ad.
+    firebase::admob::InterstitialAd *interstitial_ad = static_cast<firebase::admob::InterstitialAd*>(user_data);
+    if (future.error() == firebase::admob::kAdMobErrorNone) {
+        interstitial_ad->LoadAd(my_ad_request);
+        printLog("Interstitial init complete");
+    } else {
+        printLog("Interstitial init error");
+    }
+}
+
+static void BannerInitCallback(const firebase::Future<void>& future, void* user_data) {
+    firebase::admob::BannerView *bannerView = static_cast<firebase::admob::BannerView*>(user_data);
+    if (future.error() == firebase::admob::kAdMobErrorNone) {
+        bannerView->LoadAd(my_ad_request);
+        printLog("Banner init complete");
+    } else {
+        printLog("Banner init error");
+    }
+}
+
+static void RewardedLoadedCallback(const firebase::Future<void>& future, void* user_data) {
+    std::string *bannerId = static_cast<std::string*>(user_data);
+    delete bannerId;
+    if (future.error() == firebase::admob::kAdMobErrorNone) {
+        firebase::admob::rewarded_video::Show(getAdParent());
+        printLog("Rewarded load complete");
+    } else {
+        printLog("Rewarded load error");
+    }
+}
+
+static void RewardedInitCallback(const firebase::Future<void>& future, void* user_data) {
+    std::string *bannerId = static_cast<std::string*>(user_data);
+    if (future.error() == firebase::admob::kAdMobErrorNone) {
+        firebase::admob::rewarded_video::LoadAd(bannerId->c_str(), my_ad_request);
+        firebase::admob::rewarded_video::LoadAdLastResult().OnCompletion(RewardedLoadedCallback, bannerId);
+        printLog("Rewarded init complete");
+    } else {
+        printLog("Rewarded init error");
+    }
+}
+
 class MyBannerViewListener : public firebase::admob::BannerView::Listener {
-    int cbId;
+    CallbackFrame *cb;
 public:
-    MyBannerViewListener(int callbackId): cbId(callbackId) {}
+    MyBannerViewListener(int callbackId) {
+        cb = CallbackFrame::getById(callbackId);
+    }
 
     void OnPresentationStateChanged(firebase::admob::BannerView* banner_view, firebase::admob::BannerView::PresentationState state) override {
         // This method gets called when the banner view's presentation
         // state changes.
         printLog("[AdMob] Banner state changed");
+        JS::AutoValueVector valArr(cb->cx);
+        valArr.append(int32_to_jsval(cb->cx, state));
+        JS::HandleValueArray funcArgs = JS::HandleValueArray::fromMarkedLocation(1, valArr.begin());
+        cb->call(funcArgs);
     }
 
     void OnBoundingBoxChanged(firebase::admob::BannerView* banner_view, firebase::admob::BoundingBox box) override {
@@ -40,22 +96,38 @@ public:
         // changes.
         printLog("[AdMob] Banner size changed");
     }
+
+    ~MyBannerViewListener() {
+        delete cb;
+    }
 };
 
 class MyInterstitialAdListener: public firebase::admob::InterstitialAd::Listener {
-    int cbId;
+    CallbackFrame *cb;
 public:
-    MyInterstitialAdListener(int callbackId): cbId(callbackId) {};
+    MyInterstitialAdListener(int callbackId) {
+        cb = CallbackFrame::getById(callbackId);
+    };
 
     void OnPresentationStateChanged(firebase::admob::InterstitialAd* interstitialAd, firebase::admob::InterstitialAd::PresentationState state) override {
         printLog("[AdMob] InterstitialAd state changed");
+        JS::AutoValueVector valArr(cb->cx);
+        valArr.append(int32_to_jsval(cb->cx, state));
+        JS::HandleValueArray funcArgs = JS::HandleValueArray::fromMarkedLocation(1, valArr.begin());
+        cb->call(funcArgs);
+    }
+    
+    ~MyInterstitialAdListener() {
+        delete cb;
     }
 };
 
 class MyRewardedVideoListener: public firebase::admob::rewarded_video::Listener {
-    int cbId;
+    CallbackFrame *cb;
 public:
-    MyRewardedVideoListener(int callbackId): cbId(callbackId) {};
+    MyRewardedVideoListener(int callbackId) {
+        cb = CallbackFrame::getById(callbackId);
+    };
 
     void OnRewarded(firebase::admob::rewarded_video::RewardItem item) override {
         printLog("[AdMob] On reward item");
@@ -63,13 +135,16 @@ public:
 
     void OnPresentationStateChanged(firebase::admob::rewarded_video::PresentationState state) override {
         printLog("[AdMob] InterstitialAd state changed");
+        JS::AutoValueVector valArr(cb->cx);
+        valArr.append(int32_to_jsval(cb->cx, state));
+        JS::HandleValueArray funcArgs = JS::HandleValueArray::fromMarkedLocation(1, valArr.begin());
+        cb->call(funcArgs);
+    }
+    
+    ~MyRewardedVideoListener() {
+        delete cb;
     }
 };
-
-firebase::admob::AdParent getAdParent() {
-    // Returns the Android Activity.
-    return cocos2d::JniHelper::getActivity();
-}
 
 ///////////////////////////////////////
 //
@@ -99,7 +174,8 @@ static bool jsb_admob_show_banner(JSContext *cx, uint32_t argc, jsval *vp)
         banner_view = new firebase::admob::BannerView();
         banner_view->Initialize(getAdParent(), bannerId.c_str(), ad_size);
         banner_view->SetListener(new MyBannerViewListener(cb->callbackId));
-        banner_view->LoadAd(my_ad_request);
+        banner_view->InitializeLastResult().OnCompletion(BannerInitCallback, banner_view);
+        //banner_view->LoadAd(my_ad_request);
         rec.rval().set(JSVAL_TRUE);
         return true;
     } else {
@@ -126,7 +202,7 @@ static bool jsb_admob_show_interstitial(JSContext *cx, uint32_t argc, jsval *vp)
         interstitial_ad = new firebase::admob::InterstitialAd();
         interstitial_ad->Initialize(getAdParent(), bannerId.c_str());
         interstitial_ad->SetListener(new MyInterstitialAdListener(cb->callbackId));
-        interstitial_ad->LoadAd(my_ad_request);
+        interstitial_ad->InitializeLastResult().OnCompletion(InterstitialInitCallback, interstitial_ad);
         rec.rval().set(JSVAL_TRUE);
         return true;
     } else {
@@ -148,9 +224,12 @@ static bool jsb_admob_show_rewarded(JSContext *cx, uint32_t argc, jsval *vp)
         std::string bannerId;
         JS::RootedValue arg0Val(cx, args.get(0));
         ok &= jsval_to_std_string(cx, arg0Val, &bannerId);
+        std::string *bannerIdCopy = new std::string(bannerId);
 
-        firebase::admob::rewarded_video::LoadAd(bannerId.c_str(), my_ad_request);
+        firebase::admob::rewarded_video::Initialize();
+        firebase::admob::rewarded_video::InitializeLastResult().OnCompletion(RewardedInitCallback, bannerIdCopy);
         firebase::admob::rewarded_video::SetListener(new MyRewardedVideoListener(cb->callbackId));
+        
         rec.rval().set(JSVAL_TRUE);
         return true;
     } else {
@@ -175,7 +254,6 @@ void register_all_admob_framework(JSContext* cx, JS::HandleObject obj) {
 #endif
     // Initialize AdMob.
     firebase::admob::Initialize(*app);
-    firebase::admob::rewarded_video::Initialize();
 
     printLog("[AdMob] register js interface");
     JS::RootedObject ns(cx);
